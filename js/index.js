415ce0488462d964ca7b49197b3b9e119b616000
Shawn Huelle <shuelle@posteo.de>
2023-08-10 17:18:08 +0200

    edit js of show/hide answer

--- js/index.js
+++ js/index.js
@@ -5,4 +5,9 @@ const hiddenAnswer = document.querySelector('[data-js="hiddenAnswer"]');
 
 answerButton.addEventListener("click", () => {
   hiddenAnswer.classList.toggle("quizapp__card--answer--hidden");
+  if (!hiddenAnswer.classList.contains("quizapp__card--answer--hidden")) {
+    answerButton.textContent = "Hide Answer";
+  } else {
+    answerButton.textContent = "Show Answer";
+  }
 });
