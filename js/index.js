62f107f5a6098248012582accd28514f11bd5054
Shawn Huelle <shuelle@posteo.de>
2023-08-12 09:35:19 +0200

    broke js attempting to add structure -- not surprised -- can't fix

--- js/index.js
+++ js/index.js
@@ -1,18 +1,6 @@
 console.clear();
 
-// Answer Button Toggle!
-
-const answerButton = document.querySelector('[data-js="showAnswer"]');
-const hiddenAnswer = document.querySelector('[data-js="hiddenAnswer"]');
-
-answerButton.addEventListener("click", () => {
-  hiddenAnswer.classList.toggle("quizapp__card--answer--hidden");
-  if (!hiddenAnswer.classList.contains("quizapp__card--answer--hidden")) {
-    answerButton.textContent = "Hide Answer";
-  } else {
-    answerButton.textContent = "Show Answer";
-  }
-});
+import { answerButton } from "../components/answer__buttons.js";
 
 // Bookmark Toggle!
 
