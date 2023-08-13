d5e065c90b8758ce1c880c190d1ce3234f9570c5
Shawn Huelle <shuelle@posteo.de>
2023-08-13 11:33:06 +0200

    edit form.js

--- components/form.js
+++ components/form.js
@@ -1,3 +1,11 @@
 console.clear;
 
-button.addEventListener("", () => {});
+const form = document.querySelector('[js-data="quizappNewCardForm"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formElements = event.target.elements;
+
+  console.log(formElements.theQuestion);
+  console.log(formElements.theQuestion.value);
+});
